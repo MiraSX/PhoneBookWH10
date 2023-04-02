bc558c896a8b74411a9d679a0b0a2afb560d1de4
Anton Kalchenko <anton11042002@gmail.com>
2023-04-02 23:21:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from collections import UserDict
 
 
-class AddressBook(UserDict):
-    def add_record(self, record):
-        self.data[record.name] = record.phones
-
-    def __str__(self):
-        for name, phones in self.data.items():
-            return f'{name}: {", ".join(str(phone) for phone in phones)}'
-
-
 class Field:
     pass
 
@@ -21,7 +12,7 @@ class Name(Field):
         else:
             self.name = name
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
 
 
@@ -29,30 +20,36 @@ class Phone(Field):
     def __init__(self, phone):
         self.phone = phone
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.phone
 
 
 class Record:
-    def __init__(self, name, phone=None):
+    def __init__(self, name: Name, phone: Phone = None):
         self.name = name
         self.phone = phone
         self.phones = []
         if phone:
-            self.phones.append(phone)
+            self.phones.append(str(phone))
 
     def add_phone(self, phone):
-        self.phones.append(phone)
+        self.phones.append(str(phone))
 
     def delete_phone(self, phone):
-        self.phones.remove(phone)
+        self.phones.remove(str(phone))
 
     def change_phone(self, phone_old, phone_new):
         try:
-            self.phones.remove(phone_old)
-            self.phones.append(phone_new)
+            self.phones.remove(str(phone_old))
+            self.phones.append(str(phone_new))
         except ValueError as e:
             return e
+
+
+class AddressBook(UserDict):
+
+    def add_record(self, record: Record):
+        self.data[record.name.name] = record
 
 
 if __name__ == '__main__':
@@ -63,9 +60,10 @@ if __name__ == '__main__':
     phoneA2 = Phone('4231423')
     rec1 = Record(name1, phoneA1)
     rec1.add_phone(phoneA2)
-    phonechange1 = Phone('1111')
-    rec1.delete_phone(phoneA2)
-    rec1.change_phone(phoneA1, phonechange1)
-
     ab.add_record(rec1)
+    print(ab[name1.name])
+    print(ab[name1.name].phones)
+
+    for rec in ab.values():
+        print(rec.phones)
     print(ab)
